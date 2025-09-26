5deb4af4429d51ecd3cc080a7172b8f651b31023
ganeshghutukade-04 <your-github-ganeshghutukade@acpce.ac.in>
2025-09-26 18:40:05 +0530

    First commit with app.py and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Step 1: Build Process Started..."
+echo "Compiling the application (simulated)"
+
+echo "Step 2: Running Tests..."
+echo "All tests passed successfully!"
+
+echo "Step 3: Deploying Application..."
+echo "Application deployed successfully!"
